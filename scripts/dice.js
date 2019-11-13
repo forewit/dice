864791b3408b4f587d3320afea2edee0017f1722
Anderson <MDAnderson@samaritan.org>
2019-11-13 14:58:53 -0500

    na

--- scripts/dice.js
+++ scripts/dice.js
@@ -386,7 +386,7 @@
             ? new THREE.WebGLRenderer({ antialias: true, alpha: true })
             : new THREE.CanvasRenderer({ antialias: true, alpha: true });
         container.appendChild(this.renderer.domElement);
-        this.renderer.domElement.appendChild("Your browser does not support WebGL!");
+        this.renderer.domElement.innerHTML="Your browser does not support WebGL!";
         this.renderer.shadowMap.enabled = true;
         this.renderer.shadowMap.type = THREE.PCFShadowMap;
         this.renderer.setClearColor(0xffffff, 0);
