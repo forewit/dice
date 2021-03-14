fa3399cfdb8ad49f1a26a2dfc3d9f5c70c035304
Anderson <MDAnderson@samaritan.org>
2021-03-13 23:40:29 -0500

    added gestures

--- scripts/app.js
+++ scripts/app.js
@@ -1,79 +1,58 @@
-import { gestures } from "./gestures.js";
+let saved_dice = {
+    boost: 0,
+    setback: 0,
+    ability: 0,
+    difficulty: 0,
+    proficiency: 0,
+    challenge: 0,
+    force: 0
+}
 
-(function (global, factory) {
-    typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
-        typeof define === 'function' && define.amd ? define(['exports'], factory) :
-            (global = global || self, factory(global.app = {}));
-}(this, (function (exports) {
-    'use strict';
+for (const dice in saved_dice) {
+    let elm, counter, gesture;
 
-    let saved_dice = {
-        boost: 0,
-        setback: 0,
-        ability: 0,
-        difficulty: 0,
-        proficiency: 0,
-        challenge: 0,
-        force: 0
+    try {
+        elm = document.getElementById(dice);
+        counter = elm.children[1];
+    } catch (e) {
+        console.log('Cannot find element with ID:', dice);
+        continue
     }
-    
-    for (const dice in saved_dice) {
-        let elm, counter;
-    
-        try {
-            elm = document.getElementById(dice);
-            counter = elm.children[1];
-        } catch (e) {
-            console.log('Cannot find element with ID:', dice);
-            continue
-        }
-    
-        elm.addEventListener('contextmenu', (e) => {
-            if (saved_dice[dice] > 0) {
-                saved_dice[dice]--;
-                counter.innerHTML = saved_dice[dice]
-    
-                if (saved_dice[dice] == 0) counter.classList.add('hidden');
-            }
-            e.preventDefault()
-        })
-    
-        elm.addEventListener('click', (e) => {
-            saved_dice[dice]++
+
+    gesture = new Gestures(elm)
+    gesture.on('click tap', ()=>{
+        // increment counter and add to saved_dice
+        saved_dice[dice]++
+        counter.innerHTML = saved_dice[dice]
+        counter.classList.remove('hidden');
+    })
+    gesture.on('rightClick', ()=>{
+        // decrement counter and remove from saved_dice
+        if (saved_dice[dice] > 0) {
+            saved_dice[dice]--;
             counter.innerHTML = saved_dice[dice]
-            counter.classList.remove('hidden');
-        })
-    }
-    
-    let roll_saved_dice = function () {
-        if (saved_dice.boost || saved_dice.setback || saved_dice.ability || saved_dice.difficulty ||
-            saved_dice.proficiency || saved_dice.challenge || saved_dice.force) {
-    
-            let inputString = `${saved_dice.boost}d101 + ${saved_dice.setback}d102 + ${saved_dice.ability}d103 + ${saved_dice.difficulty}d104 + ${saved_dice.proficiency}d105 + ${saved_dice.challenge}d106 + ${saved_dice.force}d107`
-            document.getElementById("dice-box").classList.remove("hidden")
-            gg.roll_dice(inputString);
+
+            if (saved_dice[dice] == 0) counter.classList.add('hidden');
         }
-    }
-    
-    let clear_saved_dice = function () {
-        document.getElementById("dice-box").classList.add("hidden")
-    }
-    
-
-    let elm = document.getElementById('ability');
-    gestures.on('longClick', () => {
-        console.log('long click')
     })
-    gestures.on('click', () => {
-        console.log('click')
+    gesture.on('longClick longPress', ()=>{
+        // clear ounter and reset saved_dice
+        saved_dice[dice] = 0;
+        counter.classList.add('hidden');
     })
-    gestures.start(elm)
+    gesture.start();
+}
 
-    elm = document.getElementById('proficiency');
-    gestures.start(elm)
+let roll_saved_dice = function () {
+    if (saved_dice.boost || saved_dice.setback || saved_dice.ability || saved_dice.difficulty ||
+        saved_dice.proficiency || saved_dice.challenge || saved_dice.force) {
 
-    exports.clear_saved_dice = clear_saved_dice
-    exports.roll_saved_dice = roll_saved_dice
+        let inputString = `${saved_dice.boost}d101 + ${saved_dice.setback}d102 + ${saved_dice.ability}d103 + ${saved_dice.difficulty}d104 + ${saved_dice.proficiency}d105 + ${saved_dice.challenge}d106 + ${saved_dice.force}d107`
+        document.getElementById("dice-box").classList.remove("hidden")
+        gg.roll_dice(inputString);
+    }
+}
 
-    Object.defineProperty(exports, '__esModule', { value: true });
-})));
+let clear_saved_dice = function () {
+    document.getElementById("dice-box").classList.add("hidden")
+}
--- scripts/gestures.js
+++ scripts/gestures.js
@@ -1,280 +1,326 @@
-// TODO: add longpress drag & middle click drag
-
-// PREFERENCES
-let longPressDelay = 500;
-let doubleTapDelay = 300;
-let longClickDelay = 500;
-let doubleClickDelay = 300;
-
-// STATE MANAGEMENT
-let elm;
-let dragging = false;
-let pinching = false;
-let hypo = undefined;
-let taps = 0;
-let lastTouchEndTime = 0;
-let touch = { identifier: undefined };
-let mouseMoving = false;
-let clicks = 0;
-let mouseupTime = 0;
-let mouse = {};
-
-let noop = function () { };
-let callbacks;
-clear();
-
-// EXPORTS-------------------------------------------
-export let gestures = {
-    on: on,
-    off: off,
-    start: start,
-    stop: stop,
-    clear: clear,
-};
-
-function on(name, callback) { callbacks[name] = callback; }
-function off(name) { callbacks[name] = noop; }
-function clear() {
-    callbacks = {
-        // mouse callbacks
-        click: noop,
-        doubleClick: noop,
-        longClick: noop,
-        rightClick: noop,
-        wheel: noop,
-        mouseDragStart: noop,
-        mouseDragging: noop,
-        mouseDragEnd: noop,
-        // touch callbacks
-        tap: noop,
-        longPress: noop,
-        doubleTap: noop,
-        touchDragStart: noop,
-        touchDragging: noop,
-        touchDragEnd: noop,
-        pinchStart: noop,
-        pinching: noop,
-        pinchEnd: noop,
-        // window callbacks
-        blur: noop,
-    };
-}
-// END EXPORTS---------------------------------------
-
-
-function start(element) {
-    if (elm) stop();
-    elm = element;
-    elm.addEventListener('touchstart', touchstartHandler, { passive: false });
-    elm.addEventListener('mousedown', mousedownHandler, { passive: false });
-    elm.addEventListener('wheel', wheelHandler, { passive: false });
-    elm.addEventListener('contextmenu', contextmenuHandler, { passive: false });
-    window.addEventListener('blur', blurHandler);
-}
-
-function stop() {
-    elm.removeEventListener('touchstart', touchstartHandler);
-    elm.removeEventListener('mousedown', mousedownHandler);
-    elm.removeEventListener('wheel', wheelHandler);
-    elm.removeEventListener('contextmenu', contextmenuHandler);
-    window.removeEventListener('blur', blurHandler);
-}
-
-function copyTouch(newTouch) {
-    return {
-        identifier: newTouch.identifier,
-        x: newTouch.clientX,
-        y: newTouch.clientY
+(function (root, factory) {
+    if (typeof exports === 'object') {
+        module.exports = factory();
+    } else if (typeof define === 'function' && define.amd) {
+        define([], factory);
+    } else {
+        root.Gestures = factory();
     }
-}
+}(this, function () {
 
-function blurHandler(e) {
-    window.removeEventListener('mousemove', mousemoveHandler);
-    window.removeEventListener('mouseup', mouseupHandler);
+    'use strict';
 
-    mouseupTime = new Date();
+    // PREFERENCES
+    const LONG_PRESS_DELAY = 500;
+    const DOUBLE_TAP_DELAY = 300;
+    const LONG_CLICK_DELAY = 500;
+    const DOUBLE_CLICK_DELAY = 100;
 
-    if (mouseMoving) {
-        // MOUSE DRAG END DETECTION
-        callbacks.mouseDragEnd(mouse);
-    } 
+    function noop() { };
 
-    callbacks.blur();
-}
-
-function wheelHandler(e) {
-    // avoid using mouse so that wheel events don't override mouse move events
-    let point = { x: e.clientX, y: e.clientY };
-
-    // WHEEL DETECTION
-    callbacks.wheel(point, e.deltaY);
-
-    e.preventDefault();
-    e.stopPropagation();
-}
-
-function contextmenuHandler(e) { e.preventDefault(); }
-
-function mousedownHandler(e) {
-    mouseMoving = false;
-
-    window.addEventListener('mousemove', mousemoveHandler, { passive: false });
-    window.addEventListener('mouseup', mouseupHandler);
-    mouse = { x: e.clientX, y: e.clientY };
-
-    // LONGCLICK DETECTION
-    window.setTimeout(function () {
-        let now = new Date();
-        if (now - mouseupTime >= longClickDelay && !mouseMoving) {
-            window.removeEventListener('mousemove', mousemoveHandler);
-            window.removeEventListener('mouseup', mouseupHandler);
-            callbacks.longClick(mouse);
+    function copyTouch(newTouch) {
+        return {
+            identifier: newTouch.identifier,
+            x: newTouch.clientX,
+            y: newTouch.clientY
         }
-    }, longClickDelay)
+    }
 
-    e.preventDefault();
-    e.stopPropagation();
-}
+    class Gestures {
+        constructor(element) {
+            // STATE MANAGEMENT 
+            this.elm = element;
+            this.dragging = false;
+            this.pinching = false;
+            this.hypo = undefined;
+            this.taps = 0;
+            this.lastTouchEndTime = 0;
+            this.touch = { identifier: undefined };
+            this.mouseMoving = false;
+            this.clicks = 0;
+            this.mouseupTime = 0;
+            this.mouse = {};
+            this.callbacks = {};
 
-function mousemoveHandler(e) {
-    // MOUSE DRAG START DETECTION
-    if (!mouseMoving) callbacks.mouseDragStart(mouse);
+            // clear all callbacks
+            this.clear();
 
-    mouseMoving = true;
+            // bind handlers
+            this.blurHandler = this.blurHandle.bind(this);
+            this.mousedownHandler = this.mousedownHandle.bind(this);
+            this.mousemoveHandler = this.mousemoveHandle.bind(this);
+            this.mouseupHandler = this.mouseupHandle.bind(this);
+            this.contextmenuHandler = this.contextmenuHandle.bind(this);
+            this.touchstartHandler = this.touchstartHandle.bind(this);
+            this.touchmoveHandler = this.touchmoveHandle.bind(this);
+            this.touchendHandler = this.touchendHandle.bind(this);
+        }
 
-    mouse = { x: e.clientX, y: e.clientY };
+        clear() {
+            this.callbacks = {
+                // mouse callbacks
+                click: noop,
+                doubleClick: noop,
+                longClick: noop,
+                rightClick: noop,
+                wheel: noop,
+                mouseDragStart: noop,
+                mouseDragging: noop,
+                mouseDragEnd: noop,
+                // touch callbacks
+                tap: noop,
+                longPress: noop,
+                doubleTap: noop,
+                touchDragStart: noop,
+                touchDragging: noop,
+                touchDragEnd: noop,
+                pinchStart: noop,
+                pinching: noop,
+                pinchEnd: noop,
+                // window callbacks
+                blur: noop,
+            };
+        }
 
-    // MOUUSE DRAGGING DETECTION
-    callbacks.mouseDragging(mouse);
-}
+        on(name, callback) {
+            let words =  name.split(" ");
 
-function mouseupHandler(e) {
-    window.removeEventListener('mousemove', mousemoveHandler);
-    window.removeEventListener('mouseup', mouseupHandler);
+            for (let i = 0; i < words.length; i++)
+                this.callbacks[words[i]] = callback
+        }
 
-    mouseupTime = new Date();
+        off(name) {
+            let words = name.split(" ");
+        
+            for (let i = 0; i < words.length; i++)
+                this.callbacks[words[i]] = noop
+        }
 
-    if (!mouseMoving) {
-        // RIGHT CLICK DETECTION
-        if (e.which === 3 || e.button === 2) {
-            callbacks.rightClick(mouse);
-        } else {
-            // CLICK DETECTION
-            if (clicks == 0) callbacks.click(mouse);
+        start() {
+            var me = this;
 
-            // DOUBLE CLICK DETECTION
-            clicks++;
+            me.elm.addEventListener('touchstart', me.touchstartHandler, { passive: false });
+            me.elm.addEventListener('mousedown', me.mousedownHandler, { passive: false });
+            me.elm.addEventListener('wheel', me.wheelHandler, { passive: false });
+            me.elm.addEventListener('contextmenu', me.contextmenuHandler, { passive: false });
+            window.addEventListener('blur', me.blurHandler);
+        }
+
+        stop() {
+            var me = this;
+
+            me.elm.removeEventListener('touchstart', me.touchstartHandler);
+            me.elm.removeEventListener('mousedown', me.mousedownHandler);
+            me.elm.removeEventListener('wheel', me.wheelHandler);
+            me.elm.removeEventListener('contextmenu', me.contextmenuHandler);
+            window.removeEventListener('blur', me.blurHandler);
+        }
+    
+        blurHandle(e) {
+            var me = this;
+
+            window.removeEventListener('mousemove', me.mousemoveHandler);
+            window.removeEventListener('mouseup', me.mouseupHandler);
+    
+            me.mouseupTime = new Date();
+    
+            if (me.mouseMoving) {
+                // MOUSE DRAG END DETECTION
+                me.callbacks.mouseDragEnd(me.mouse);
+            }
+    
+            me.callbacks.blur();
+        }
+    
+        wheelHandle(e) {
+            // avoid using this.mouse so that wheel events don't override mouse move events
+            let point = { x: e.clientX, y: e.clientY };
+    
+            // WHEEL DETECTION
+            this.callbacks.wheel(point, e.deltaY);
+    
+            e.preventDefault();
+            e.stopPropagation();
+        }
+    
+        contextmenuHandle(e) { e.preventDefault(); }
+    
+        mousedownHandle(e) {
+            var me = this;
+
+            me.mouseMoving = false;
+    
+            window.addEventListener('mousemove', me.mousemoveHandler, { passive: false });
+            window.addEventListener('mouseup', me.mouseupHandler);
+            me.mouse = { x: e.clientX, y: e.clientY };
+    
+            // LONGCLICK DETECTION
             window.setTimeout(function () {
-                if (clicks > 1) callbacks.doubleClick(mouse);
-                clicks = 0;
-            }, doubleClickDelay);
+                let now = new Date();
+                if (now - me.mouseupTime >= LONG_CLICK_DELAY && !me.mouseMoving) {
+                    window.removeEventListener('mousemove', me.mousemoveHandler);
+                    window.removeEventListener('mouseup', me.mouseupHandler);
+                    me.callbacks.longClick(me.mouse);
+                }
+            }, LONG_CLICK_DELAY)
+    
+            e.preventDefault();
+            e.stopPropagation();
         }
-    } else {
-        // MOUSE DRAG END DETECTION
-        callbacks.mouseDragEnd(mouse);
-    }
-}
+    
+        mousemoveHandle(e) {
+            var me = this;
 
-function touchstartHandler(e) {
-    e.preventDefault();
-    e.stopPropagation();
-
-    // check if two touches were started simultaneously
-    if (e.targetTouches.length > 1) {
-        if (e.targetTouches[0] == touch.identifier) return;
-        else pinching = true;
-    }
-
-    window.addEventListener('touchmove', touchmoveHandler, { passive: false });
-    window.addEventListener('touchend', touchendHandler);
-    window.addEventListener('touchcancel', touchendHandler);
-
-    // update primary touch location
-    touch = copyTouch(e.targetTouches[0]);
-
-    // LONGPRESS DETECTION
-    window.setTimeout(function () {
-        // cancel long press if in the middle of a gesture
-        if (dragging || pinching) return;
-
-        // verify the touch hasn't been released
-        let now = new Date();
-        if (now - lastTouchEndTime >= longPressDelay) {
-            window.removeEventListener('touchmove', touchmoveHandler);
-            window.removeEventListener('touchend', touchendHandler);
-            window.removeEventListener('touchcancel', touchendHandler);
-            dragging = false;
-            pinching = false;
-            hypo = undefined;
-
-            callbacks.longPress(touch);
+            // MOUSE DRAG START DETECTION
+            if (!me.mouseMoving) me.callbacks.mouseDragStart(me.mouse);
+    
+            me.mouseMoving = true;
+    
+            me.mouse = { x: e.clientX, y: e.clientY };
+    
+            // MOUUSE DRAGGING DETECTION
+            me.callbacks.mouseDragging(me.mouse);
         }
-    }, longPressDelay);
-}
+    
+        mouseupHandle(e) {
+            var me = this;
 
-function touchmoveHandler(e) {
-    e.preventDefault();
-    e.stopPropagation();
-
-    if (dragging) {
-        touch = copyTouch(e.targetTouches[0]);
-        callbacks.touchDragging(touch);
-        return;
-
-    } else if (pinching || e.targetTouches.length > 1) {
-        touch = copyTouch(e.targetTouches[0]);
-        let touch2 = copyTouch(e.targetTouches[1]);
-        let center = {
-            x: (touch.x + touch2.x) / 2,
-            y: (touch.y + touch2.y) / 2
+            window.removeEventListener('mousemove', me.mousemoveHandler);
+            window.removeEventListener('mouseup', me.mouseupHandler);
+    
+            me.mouseupTime = new Date();
+    
+            if (!me.mouseMoving) {
+                // RIGHT CLICK DETECTION
+                if (e.which === 3 || e.button === 2) {
+                    me.callbacks.rightClick(me.mouse);
+                } else {
+                    // CLICK DETECTION
+                    if (me.clicks == 0) me.callbacks.click(me.mouse);
+    
+                    // DOUBLE CLICK DETECTION
+                    me.clicks++;
+                    window.setTimeout(function () {
+                        if (me.clicks > 1) me.callbacks.doubleClick(me.mouse);
+                        me.clicks = 0;
+                    }, DOUBLE_CLICK_DELAY);
+                }
+            } else {
+                // MOUSE DRAG END DETECTION
+                me.callbacks.mouseDragEnd(me.mouse);
+            }
         }
+    
+        touchstartHandle(e) {
+            var me = this;
 
-        let hypo1 = Math.hypot((touch.x - touch2.x), (touch.y - touch2.y));
-        if (hypo === undefined) {
-            hypo = hypo1;
-            callbacks.pinchStart(center);
+            e.preventDefault();
+            e.stopPropagation();
+    
+            // check if two touches were started simultaneously
+            if (e.targetTouches.length > 1) {
+                if (e.targetTouches[0] == me.touch.identifier) return;
+                else me.pinching = true;
+            }
+    
+            window.addEventListener('touchmove', me.touchmoveHandler, { passive: false });
+            window.addEventListener('touchend', me.touchendHandler);
+            window.addEventListener('touchcancel', me.touchendHandler);
+    
+            // update primary touch location
+            me.touch = copyTouch(e.targetTouches[0]);
+    
+            // LONGPRESS DETECTION
+            window.setTimeout(function () {
+                // cancel long press if in the middle of a gesture
+                if (dragging || pinching) return;
+    
+                // verify the touch hasn't been released
+                let now = new Date();
+                if (now - me.lastTouchEndTime >= LONG_PRESS_DELAY) {
+                    window.removeEventListener('touchmove', me.touchmoveHandler);
+                    window.removeEventListener('touchend', me.touchendHandler);
+                    window.removeEventListener('touchcancel', me.touchendHandler);
+                    me.dragging = false;
+                    me.pinching = false;
+                    me.hypo = undefined;
+    
+                    me.callbacks.longPress(me.touch);
+                }
+            }, LONG_PRESS_DELAY);
         }
+    
+        touchmoveHandle(e) {
+            var me = this;
 
-        pinching = true;
-        callbacks.pinching(center, hypo1 / hypo);
-        hypo = hypo1;
-        return;
-    } else {
-        dragging = true;
-        callbacks.touchDragStart(touch);
-        touch = copyTouch(e.targetTouches[0]);
-        callbacks.touchDragging(touch);
+            e.preventDefault();
+            e.stopPropagation();
+    
+            if (me.dragging) {
+                me.touch = copyTouch(e.targetTouches[0]);
+                me.callbacks.touchDragging(me.touch);
+                return;
+    
+            } else if (me.pinching || e.targetTouches.length > 1) {
+                me.touch = copyTouch(e.targetTouches[0]);
+                let touch2 = copyTouch(e.targetTouches[1]);
+                let center = {
+                    x: (me.touch.x + touch2.x) / 2,
+                    y: (me.touch.y + touch2.y) / 2
+                }
+    
+                let hypo1 = Math.hypot((me.touch.x - touch2.x), (me.touch.y - touch2.y));
+                if (me.hypo === undefined) {
+                    me.hypo = hypo1;
+                    me.callbacks.pinchStart(me.center);
+                }
+    
+                me.pinching = true;
+                me.callbacks.pinching(center, hypo1 / me.hypo);
+                me.hypo = hypo1;
+                return;
+            } else {
+                me.dragging = true;
+                me.callbacks.touchDragStart(me.touch);
+                me.touch = copyTouch(e.targetTouches[0]);
+                me.callbacks.touchDragging(me.touch);
+            }
+        }
+    
+        touchendHandle(e) {
+            var me = this;
+
+            if (me.dragging &&
+                e.targetTouches.length > 0 &&
+                e.targetTouches[0].identifier == me.touch.identifier) {
+                return;
+            }
+    
+            me.lastTouchEndTime = new Date();
+            window.removeEventListener('touchmove', me.touchmoveHandler);
+            window.removeEventListener('touchend', me.touchendHandler);
+            window.removeEventListener('touchcancel', me.touchendHandler);
+    
+            if (me.dragging) {
+                me.dragging = false;
+                me.callbacks.touchDragEnd();
+            } else if (me.pinching) {
+                me.pinching = false;
+                me.hypo = undefined;
+                me.callbacks.pinchEnd()
+            } else {
+                // TAP DETECTION
+                if (me.taps == 0) me.callbacks.tap(me.touch);
+    
+                // DOUBLE TAP DETECTION
+                me.taps++;
+                window.setTimeout(function () {
+                    if (me.taps > 1) me.callbacks.doubleTap(me.touch);
+                    me.taps = 0;
+                }, DOUBLE_TAP_DELAY);
+            }
+        }
     }
-}
 
-function touchendHandler(e) {
-    if (dragging &&
-        e.targetTouches.length > 0 &&
-        e.targetTouches[0].identifier == touch.identifier) {
-        return;
-    }
-
-    lastTouchEndTime = new Date();
-    window.removeEventListener('touchmove', touchmoveHandler);
-    window.removeEventListener('touchend', touchendHandler);
-    window.removeEventListener('touchcancel', touchendHandler);
-
-    if (dragging) {
-        dragging = false;
-        callbacks.touchDragEnd();
-    } else if (pinching) {
-        pinching = false;
-        hypo = undefined;
-        callbacks.pinchEnd()
-    } else {
-        // TAP DETECTION
-        if (taps == 0) callbacks.tap(touch);
-
-        // DOUBLE TAP DETECTION
-        taps++;
-        window.setTimeout(function () {
-            if (taps > 1) callbacks.doubleTap(touch);
-            taps = 0;
-        }, doubleTapDelay);
-    }
-}
+    return Gestures;
+}));
